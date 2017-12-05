0327f538be964c5109d1c3e2b586e1db2addda07
luo <wangge0101@126.com>
2017-12-05 18:07:19 +0800

    注册了admin以及添加了表的显示中文

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from course.models import Student, Teacher, Course, Enrollment
+
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Enrollment)
--- course/models.py
+++ course/models.py
@@ -9,9 +9,21 @@ class Student(models.Model):
         verbose_name="性别"
     )
 
+    class Meta:
+        verbose_name = verbose_name_plural = "学生表"
+
+    def __str__(self):
+        return self.name
+
 
 class Teacher(models.Model):
     name = models.CharField(max_length=20, verbose_name="老师名")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "老师表"
+
+    def __str__(self):
+        return self.name
 
 
 class Course(models.Model):
@@ -19,7 +31,19 @@ class Course(models.Model):
     room = models.CharField(max_length=20, verbose_name="教室名")
     teacher = models.ForeignKey(to=Teacher, verbose_name="老师", related_name="courses", on_delete=None)
 
+    class Meta:
+        verbose_name = verbose_name_plural = "课程表"
+
+    def __str__(self):
+        return self.name
+
 
 class Enrollment(models.Model):
     student = models.ForeignKey(to=Student, on_delete=None, related_name="stu_enroll")
     course = models.ForeignKey(to=Course, on_delete=None, related_name="course_enroll")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "选课表"
+
+    def __str__(self):
+        return "选课表"
