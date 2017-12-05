d5e0b980df0f5eb82e2f292254963c7e07856962
luo <wangge0101@126.com>
2017-12-05 17:55:28 +0800

    创建了数据库并添加了一张选课表

--- course/models.py
+++ course/models.py
@@ -18,3 +18,8 @@ class Course(models.Model):
     name = models.CharField(max_length=20, verbose_name="课程名")
     room = models.CharField(max_length=20, verbose_name="教室名")
     teacher = models.ForeignKey(to=Teacher, verbose_name="老师", related_name="courses", on_delete=None)
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(to=Student, on_delete=None, related_name="stu_enroll")
+    course = models.ForeignKey(to=Course, on_delete=None, related_name="course_enroll")
